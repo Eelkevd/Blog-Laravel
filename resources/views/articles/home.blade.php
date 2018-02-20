d9cc261b5b4f8aa757772dbac33f026071c8bc11
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-02-20 11:59:03 +0100

    W3-002 layout les 10 laracasts

--- resources/views/articles/home.blade.php
+++ resources/views/articles/home.blade.php
@@ -1,22 +1,7 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-           
-        </style>
-    </head>
-    <body>
+@extends ('layouts.layout')
         
+    @section ('content')
 
         <ul>
 
@@ -27,5 +12,5 @@
             @endforeach 
 
         </ul>
-    </body>
-</html>
+
+    @endsection
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,23 +1,8 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+@extends ('layouts.layout')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    @section ('content')
 
-        <!-- Styles -->
-        <style>
-           
-        </style>
-    </head>
-    <body>
-        
                 <h1>{{ $article->bodytext }}</h1>
 
-    </body>
-</html>
+    @endsection
