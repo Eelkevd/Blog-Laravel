8d360d2499335fa6ba4fec2cd224ca3186c7fa09
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-02-21 14:36:40 +0100

    heroku test 2

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title> Blog2 </title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link type="text/css" rel="stylesheet" href="/css/blog2.css" />
+
+        <!-- Styles -->
+        <style>
+           
+        </style>
+    </head>
+    <body>
+
+
+        @yield('header')
+
+        @include('Layouts.header')
+
+        
+
+        @yield('content')
+
+        @include('Layouts.footer')
+
+    </body>
+</html>
