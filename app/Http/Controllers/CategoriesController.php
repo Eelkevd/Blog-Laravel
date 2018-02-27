1fc15af88bbf61e4bdeb909b3dc8ea178e9378a5
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-02-27 15:05:32 +0100

    W6-002: added right for users and bloggers who are logged in

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,6 +8,12 @@ use Illuminate\Http\Request;
 
 class CategoriesController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     public function home(Category $category)
    	{
    		$articles = $category->articles;
