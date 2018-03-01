1460ac52c8350a3b4e62640aafa20fd4b6e2965d
Esmeralda <esmeraldatijhoff@yahoo.com>
2018-03-01 20:39:55 +0100

    Sidebar created

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,32 @@
+<div class="sidebar-module">
+    <h4>Archives</h4>
+      <ol class="list-unstyled">
+
+        @foreach ( $archives as $stats )
+
+          <li>
+              <a href="#">
+                {{ $stats['month'] . ' ' .  $stats['year'] }}
+              </a>
+
+          </li>
+
+        @endforeach
+
+      </ol>
+  </div>
+
+  <div class="sidebar-module">
+    <h4>Check our:</h4>
+
+    <ol class="list-unstyled">
+
+      <li><a href="https://trello.com/b/FlXnsQWy/blog-laravel-week-7" target="_blank">
+        Trello</a></li>
+      <li><a href="https://sheltered-reef-90572.herokuapp.com/articles/home" target="_blank">
+        Heroku</a></li>
+      <li><a href="https://github.com/Eelkevd/Blog-Laravel/" target="_blank">
+        Github</a></li>
+
+    </ol>
+  </div>
