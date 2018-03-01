5708fd665316a0a072459d68dd73803cc88f72ff
Esmeralda <esmeraldatijhoff@yahoo.com>
2018-03-01 09:11:04 +0100

    filling the excell old style

--- app/Paywall.php
+++ app/Paywall.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+// composer require phpoffice/phpspreadsheet -> get program
+// https://phpspreadsheet.readthedocs.io/en/develop/ -> Documentation
+
 require 'vendor/autoload.php';
 
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
@@ -21,12 +24,25 @@ class Paywall extends Model
       $spreadsheet = new Spreadsheet();
       $sheet = $spreadsheet->getActiveSheet();
       $sheet->setCellValue('A1', 'IBAN');
+      $sheet->setCellValue('B1', 'BIC');
+      $sheet->setCellValue('C1', 'mandaatid');
+      $sheet->setCellValue('D1', 'mandaatdatum');
+      $sheet->setCellValue('E1', 'bedrag');
+      $sheet->setCellValue('F1', 'naam');
+      $sheet->setCellValue('G1', 'beschrijving');
+      $sheet->setCellValue('H1', 'endtoendid');
+      $sheet->setCellValue('A2', 'IBAN');
+      $sheet->setCellValue('B2', 'BIC');
+      $sheet->setCellValue('C2', 'mandaatid');
+      $sheet->setCellValue('D2', 'mandaatdatum');
+      $sheet->setCellValue('E2', 'bedrag');
+      $sheet->setCellValue('F2', 'naam');
+      $sheet->setCellValue('G2', 'beschrijving');
+      $sheet->setCellValue('H2', 'endtoendid');
 
       $writer = new Xlsx($spreadsheet);
       $writer-<save('bills/firstbill.xlsx');
-      
+
     }
-
-
 
 }
