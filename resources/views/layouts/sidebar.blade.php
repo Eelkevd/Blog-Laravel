2fc07b93191a36f2e042bb3b415cf813c0dbddff
Eelkevd <34510065+Eelkevd@users.noreply.github.com>
2018-03-02 15:13:37 +0100

    Hopefully fixed year issue

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,7 +3,16 @@
       <h4>Archives</h4>
         <ol class="list-unstyled">
 
+          @foreach ( $archives as $stats )
 
+            <li>
+                <a href="/articles/?month={{ $stats['month'] }}&year={{ $stats['year'] }}">
+                  {{ $stats['month'] . ' ' .  $stats['year'] }}
+                </a>
+
+            </li>
+
+          @endforeach
 
         </ol>
     </div>
