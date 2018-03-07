27bdca29bd372e325f9bcb0caea06c9527925310
Esmeralda <esmeraldatijhoff@yahoo.com>
2018-03-07 09:46:41 +0100

    working pageount and [ath

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -58,57 +58,5 @@
         	</div>
 
         </div>
-        <script>
 
-          $(document).ready(function() {
-            var currentDocumentTimestamp = new Date().getTime();
-            var currentPage = window.location.pathname;
-
-
-            // if (currentPage = window.location.pathname) {
-            //   mouseEvents();
-            // }
-            // mouseEvents();
-
-
-
-
-            // if(Local.pagetime){
-            //
-            // }
-            //var myVar = setInterval(myTimer, 1000);
-
-          //   function myTimer() {
-          //     var d = new Date();
-          //     document.getElementById("demo").innerHTML = d.toLocaleTimeString()
-          //     ;
-          //
-          //       var visitedTime = myVar;
-          //            clearInterval(myVar);
-          //            alert('Test');
-          //
-          //
-          // }
-            // document.getElementById("demo").innerHTML = visitedTime.toLocaleTimeString();
-          });
-
-
-
-          function mouseEvents(){
-            window.onmousemove = document.getElementById("demo").innerHTML = 'IK BEWEEG'; // catches mouse movements
-            // window.onmousedown = updateActive(); // catches mouse movements
-            // window.onclick = updateActive();     // catches mouse clicks
-        //     window.onscroll = document.getElementById("demo").innerHTML = 'IK BEWEEG niet';    // catches scrolling
-            // window.onkeypress = updateActive();
-          }
-
-          function updateActive(){
-            var activeTime = new Date().getTime();
-            alert(activeTime);
-          }
-
-          mouseEvents();
-
-
-        </script>
     @endsection
